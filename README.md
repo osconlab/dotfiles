f29e84dcccecdf14e3a08f1a8bb5db2b7bfcc16d
osconlab <129285747+osconlab@users.noreply.github.com>
2023-03-29 14:30:12 +0200

    up

---
+++ README.md
@@ -0,0 +1,15 @@
+# Installation
+### Download and change folder
+```
+cd ~
+git clone https://github.com/z3r0bu6/dotfiles.git
+
+cd dotfiles
+```
+### Run intstall
+```
+./install.sh
+Or 
+bash install.sh
+```
+
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
+ if hash vim-addon  2>/dev/null; then
+         echo "vim-addon (vim-scripts)  installed"
+     else
+         echo "vim-addon (vim-scripts) not installed, running 'sudo yum update; sudo apt install bc'"
+         sudo apt update; sudo apt install vim-scripts
+fi
+
+echo "Installed"
